3d1117fb1034b0bcbb5e269efeafa16e9e65c9ad
lai chung ho <101287576+chung900811@users.noreply.github.com>
2022-04-26 21:41:07 -0700

    Update spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -3,7 +3,6 @@ const spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ']
 const spinner2 = function(){
   let timer = 0;
   for (let i = 0; i <= spinner.length * 2; i++){
-    // console.log(i, i % 4)
     setTimeout(() => {
       process.stdout.write(spinner[i % 4])
     }, timer)
